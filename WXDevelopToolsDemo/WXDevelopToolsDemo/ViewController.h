89222390bd1b2520bacb85f1d10fae47d13cd04c
vencentle <vencentle@gmail.com>
2016-08-23 11:22:24 +0800

    init proj

---
+++ WXDevelopToolsDemo/WXDevelopToolsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXDevelopToolsDemo
+//
+//  Created by vencentle on 16/8/23.
+//  Copyright © 2016年 vencentle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WXDevelopToolsDemo/WXDevelopToolsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WXDevelopToolsDemo
+//
+//  Created by vencentle on 16/8/23.
+//  Copyright © 2016年 vencentle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
